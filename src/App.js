f8b4c4371076de9e4fec28db01829bff9551b979
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2020-11-24 20:05:52 +0000

    styling changes to landing page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./images/spiral.svg";
 import "./App.css";
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello!</p>
+        <p>Hello Daddy!</p>
       </header>
     </div>
   );
